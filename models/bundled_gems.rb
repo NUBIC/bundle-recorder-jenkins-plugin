d4ccbdc8380b14d64b9ea1cc01a39a7165bf54b6
Rhett Sutphin <rhett@detailedbalance.net>
2012-03-16 02:33:42 -0500

    Add extremely crude views.
    For some reason, the ruby plugin API is refusing to allow me to use stapler
    views for the build action; it is forcing the action into rack integration
    mode. I'm not sure how to get the rest of Jenkins' chrome up in this mode.
    
    This implementation will suffice for a demo, though.

--- models/bundled_gems.rb
+++ models/bundled_gems.rb
@@ -1,16 +1,52 @@
+# For some reason, the ruby API is a) forcing this Action to be used
+# as a Rack handler and b) not loading rack before jruby-rack. This is
+# enough of rack to get it to render.
+module Rack
+  VERSION = [1, 1]
+
+  def self.release
+    "1.3"
+  end
+end
+
 class BundledGems
   include Jenkins::Model::Action
 
-  display_name 'Bundled Gems'
-  url_path 'bundled_gems'
+  display_name 'Bundle Gem Changes'
+  url_path 'bundledGems'
   icon 'fingerprint.png'
 
-  attr_accessor :archived_lockfile
+  attr_reader :archived_lockfile
+  attr_reader :previous_lockfile
 
-  def initialize(archived_lockfile)
+  def initialize(archived_lockfile, previous_lockfile=nil)
     super()
 
-    @archived_lockfile = archived_lockfile.to_s
+    @archived_lockfile = archived_lockfile
+    @previous_lockfile = previous_lockfile
+  end
+
+  def call(env)
+    [200, { 'Content-Type' => 'text/html' }, body]
+  end
+
+  def body
+    if previous_lockfile
+      differences = `diff '#{previous_lockfile}' '#{archived_lockfile}'`.strip
+      if differences.empty?
+        [
+          "No differences"
+        ]
+      else
+        [
+          `colordiff -u '#{previous_lockfile}' '#{archived_lockfile}' | ansifilter -H`
+        ]
+      end
+    else
+      [
+        'No previous gems'
+      ]
+    end
   end
 end
 
--- models/lockfile_recorder.rb
+++ models/lockfile_recorder.rb
@@ -9,7 +9,18 @@ class LockfileRecorder < Jenkins::Tasks::Publisher
       listener.info("Recording bundle to #{archive_target}.")
       lockfile.copyTo(archive_target)
 
-      build.native.add_action(Jenkins.plugin.export(BundledGems.new(archive_target)))
+      previous_lockfile =
+        if build.native.previous_build
+          previous_gems = build.native.
+            previous_build.actions.find { |a| a.class.to_s =~ /BundledGemsProxy/ }
+          if previous_gems
+            previous_gems.getTarget.archived_lockfile
+          end
+        end
+
+      build.native.add_action(export(
+          BundledGems.new(archive_target, previous_lockfile)
+          ))
     else
       listener.error("No bundle lock present to record. Was expecting #{lockfile}.")
       build.native.result = Java.hudson.model.Result::FAILURE
@@ -26,4 +37,9 @@ class LockfileRecorder < Jenkins::Tasks::Publisher
     build.native.workspace.child('Gemfile.lock')
   end
   private :built_lockfile
+
+  def export(a)
+    Jenkins.plugin.export(a)
+  end
+  private :export
 end
